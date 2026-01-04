305a666b6a00708fd168e80bbd1784cd2ce843ab
Chris <Chris.m.simon1@gmail.com>
2026-01-03 21:26:40 -0500

    Day 2 - Challenge 1

---
+++ day2_initials/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println(" CCC   SSS ");
+        System.out.println("C   C S   S");
+        System.out.println("C     S    ");
+        System.out.println("C      SSS ");
+        System.out.println("C         S");
+        System.out.println("C   C S   S");
+        System.out.println(" CCC   SSS ");
+    }
+}
